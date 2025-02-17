ce120da876867b68f8c967d205f4b322119d3921
fzsf163 <yofaisol619@gmail.com>
2025-02-17 23:12:47 +0600

    general error added to /user

--- src/routes/user/route.tsx
+++ src/routes/user/route.tsx
@@ -1,9 +1,11 @@
+import GeneralError from '@/features/errors/general-error'
 import NotFoundError from '@/features/errors/not-found-error'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/user')({
   component: RouteComponent,
   notFoundComponent: NotFoundError,
+  errorComponent: GeneralError,
 })
 
 function RouteComponent() {
